a6f9c1b7c6bbbcb4ce65f3eef92e6f2d94bdceb6
LAPTOP-UVQVGO40\Studium <r.w@hotmail.ch>
2023-11-15 19:51:53 +0100

    Changes in Argument Parsing

--- main.py
+++ main.py
@@ -1,54 +1,43 @@
-import argparse
 from datetime import datetime
 import torch
 from pytorch_lightning import Trainer, seed_everything
 from glue_module import GLUEDataModule, GLUETransformer
-
+import argparse
 def main(args):
     seed_everything(42)
 
     data_module = GLUEDataModule(
-        model_name_or_path=args.model_name_or_path,
-        task_name=args.task_name,
-        max_seq_length=args.max_seq_length,
-        train_batch_size=args.train_batch_size,
-        eval_batch_size=args.eval_batch_size
+        model_name_or_path='distilbert-base-uncased',
+        task_name="mrpc",
+        max_seq_length=128,
+        train_batch_size=32,
+        eval_batch_size=32
     )
 
     model = GLUETransformer(
-        model_name_or_path=args.model_name_or_path,
-        num_labels=data_module.num_labels,
-        task_name=args.task_name,
+        model_name_or_path='distilbert-base-uncased',
+        num_labels=2,
+        task_name="mrpc",
         learning_rate=args.learning_rate,
         adam_epsilon=args.adam_epsilon,
         warmup_steps=args.warmup_steps,
-        weight_decay=args.weight_decay,
-        train_batch_size=args.train_batch_size,
-        eval_batch_size=args.eval_batch_size
-        #eval_splits=data_module.eval_splits
+        weight_decay=0.0,
+        train_batch_size=32,
+        eval_batch_size=32
     )
 
     trainer = Trainer(
-        default_root_dir=args.checkpoint_dir,
+        default_root_dir="models",
         gpus=1 if torch.cuda.is_available() else 0,
-        max_epochs=args.max_epochs,
-        #progress_bar_refresh_rate=1,
+        max_epochs=3,
     )
     trainer.fit(model, data_module)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Train GLUE Transformer model.")
-    parser.add_argument("--model_name_or_path", type=str, required=True, help="Pretrained model name or path.")
-    parser.add_argument("--task_name", type=str, default="mrpc", help="GLUE task name.")
-    parser.add_argument("--max_seq_length", type=int, default=128, help="Maximum sequence length.")
-    parser.add_argument("--train_batch_size", type=int, default=32, help="Training batch size.")
-    parser.add_argument("--eval_batch_size", type=int, default=32, help="Evaluation batch size.")
-    parser.add_argument("--learning_rate", type=float, default=2e-5, help="Learning rate.")
-    parser.add_argument("--adam_epsilon", type=float, default=1e-8, help="Adam epsilon.")
-    parser.add_argument("--warmup_steps", type=int, default=0, help="Number of warmup steps.")
-    parser.add_argument("--weight_decay", type=float, default=0.0, help="Weight decay.")
-    parser.add_argument("--max_epochs", type=int, default=3, help="Maximum number of training epochs.")
-    parser.add_argument("--checkpoint_dir", type=str, default="models", help="Directory to save checkpoints.")
+    parser.add_argument("--learning_rate", type=float, default=2e-5)
+    parser.add_argument("--adam_epsilon", type=float, default=1e-8)
+    parser.add_argument("--warmup_steps", type=int, default=0)
 
     args = parser.parse_args()
     main(args)
